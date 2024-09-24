df15331a91542279bebb1b187535aec75b524ade
unknown <aladynjr@gmail.com>
2024-09-24 17:02:58 +0100

    make consolidating more effecient

--- consolidate_into_single_csv.js
+++ consolidate_into_single_csv.js
@@ -16,11 +16,16 @@ fs.readdir(folderPath, (err, files) => {
 
     const jsonFiles = files.filter(file => file.endsWith('.json') && file !== 'last_processed_info.json');
     let consolidatedData = [];
+    let twoColumnsData = [];
 
     jsonFiles.forEach(file => {
         const filePath = path.join(folderPath, file);
         const fileData = JSON.parse(fs.readFileSync(filePath, 'utf8'));
         consolidatedData = consolidatedData.concat(fileData);
+        twoColumnsData = twoColumnsData.concat(fileData.map(item => ({
+            saashub_url: item.url,
+            Website: item.Website
+        })));
     });
 
     const json2csvParser = new Parser();
@@ -28,12 +33,6 @@ fs.readdir(folderPath, (err, files) => {
 
     fs.writeFileSync(outputCsvPath, csv, 'utf8');
     console.log('CSV file has been saved as all.csv');
-
-    // Create all_two_columns.csv with only "saashub_url" and "Website" columns
-    const twoColumnsData = consolidatedData.map(item => ({
-        saashub_url: item.url,
-        Website: item.Website
-    }));
 
     const json2csvTwoColumnsParser = new Parser({ fields: ['saashub_url', 'Website'] });
     const csvTwoColumns = json2csvTwoColumnsParser.parse(twoColumnsData);
