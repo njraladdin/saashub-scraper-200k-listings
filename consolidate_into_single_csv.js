75790103c0f48b3ce9d1fbc3714baede34ee7690
unknown <aladynjr@gmail.com>
2024-09-24 17:04:34 +0100

    make consolidating more effecient

--- consolidate_into_single_csv.js
+++ consolidate_into_single_csv.js
@@ -7,8 +7,9 @@ const folderPath = path.join(__dirname, 'scraping_results');
 const outputCsvPath = path.join(__dirname, 'scraping_results', 'all.csv');
 const outputTwoColumnsCsvPath = path.join(__dirname, 'scraping_results', 'all_two_columns.csv');
 const outputZipPath = path.join(__dirname, 'scraping_results', 'all.zip');
+const BATCH_SIZE = 10; // Number of files to process at a time
 
-fs.readdir(folderPath, (err, files) => {
+fs.readdir(folderPath, async (err, files) => {
     if (err) {
         console.error('Error reading directory:', err);
         return;
@@ -18,15 +19,24 @@ fs.readdir(folderPath, (err, files) => {
     let consolidatedData = [];
     let twoColumnsData = [];
 
-    jsonFiles.forEach(file => {
-        const filePath = path.join(folderPath, file);
-        const fileData = JSON.parse(fs.readFileSync(filePath, 'utf8'));
-        consolidatedData = consolidatedData.concat(fileData);
-        twoColumnsData = twoColumnsData.concat(fileData.map(item => ({
-            saashub_url: item.url,
-            Website: item.Website
-        })));
-    });
+    for (let i = 0; i < jsonFiles.length; i += BATCH_SIZE) {
+        const batchFiles = jsonFiles.slice(i, i + BATCH_SIZE);
+        let batchData = [];
+        let batchTwoColumnsData = [];
+
+        batchFiles.forEach(file => {
+            const filePath = path.join(folderPath, file);
+            const fileData = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+            batchData = batchData.concat(fileData);
+            batchTwoColumnsData = batchTwoColumnsData.concat(fileData.map(item => ({
+                saashub_url: item.url,
+                Website: item.Website
+            })));
+        });
+
+        consolidatedData = consolidatedData.concat(batchData);
+        twoColumnsData = twoColumnsData.concat(batchTwoColumnsData);
+    }
 
     const json2csvParser = new Parser();
     const csv = json2csvParser.parse(consolidatedData);
