9a58456b35f061defbd9065cfee0d0ea14986f8f
unknown <aladynjr@gmail.com>
2024-09-24 17:24:52 +0100

    make consolidating more effecient

--- consolidate_into_single_csv.js
+++ consolidate_into_single_csv.js
@@ -1,71 +1,79 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 const path = require('path');
-const { Parser } = require('json2csv');
+const { createReadStream, createWriteStream } = require('fs');
+const { Transform } = require('stream');
+const { createGunzip } = require('zlib');
+const { stringify } = require('csv-stringify');
 const archiver = require('archiver');
 
 const folderPath = path.join(__dirname, 'scraping_results');
-const outputCsvPath = path.join(__dirname, 'scraping_results', 'all.csv');
-const outputTwoColumnsCsvPath = path.join(__dirname, 'scraping_results', 'all_two_columns.csv');
-const outputZipPath = path.join(__dirname, 'scraping_results', 'all.zip');
-const BATCH_SIZE = 10; // Number of files to process at a time
+const outputCsvPath = path.join(folderPath, 'all.csv');
+const outputTwoColumnsCsvPath = path.join(folderPath, 'all_two_columns.csv');
+const outputZipPath = path.join(folderPath, 'all.zip');
 
-fs.readdir(folderPath, async (err, files) => {
-    if (err) {
-        console.error('Error reading directory:', err);
-        return;
+async function* readJsonFiles(directory) {
+  const files = await fs.readdir(directory);
+  const jsonFiles = files.filter(file => file.endsWith('.json') && file !== 'last_processed_info.json');
+  
+  for (const file of jsonFiles) {
+    const filePath = path.join(directory, file);
+    const fileContent = await fs.readFile(filePath, 'utf8');
+    const jsonData = JSON.parse(fileContent);
+    
+    for (const item of jsonData) {
+      yield item;
     }
+  }
+}
 
-    const jsonFiles = files.filter(file => file.endsWith('.json') && file !== 'last_processed_info.json');
-    let consolidatedData = [];
-    let twoColumnsData = [];
+async function processData() {
+  const csvStream = stringify({ header: true });
+  const csvOutput = createWriteStream(outputCsvPath);
+  csvStream.pipe(csvOutput);
 
-    for (let i = 0; i < jsonFiles.length; i += BATCH_SIZE) {
-        const batchFiles = jsonFiles.slice(i, i + BATCH_SIZE);
-        let batchData = [];
-        let batchTwoColumnsData = [];
+  const twoColumnsCsvStream = stringify({ header: true });
+  const twoColumnsCsvOutput = createWriteStream(outputTwoColumnsCsvPath);
+  twoColumnsCsvStream.pipe(twoColumnsCsvOutput);
 
-        batchFiles.forEach(file => {
-            const filePath = path.join(folderPath, file);
-            const fileData = JSON.parse(fs.readFileSync(filePath, 'utf8'));
-            batchData = batchData.concat(fileData);
-            batchTwoColumnsData = batchTwoColumnsData.concat(fileData.map(item => ({
-                saashub_url: item.url,
-                Website: item.Website
-            })));
-        });
+  let count = 0;
+  for await (const item of readJsonFiles(folderPath)) {
+    csvStream.write(item);
+    twoColumnsCsvStream.write({
+      saashub_url: item.url,
+      Website: item.Website
+    });
 
-        consolidatedData = consolidatedData.concat(batchData);
-        twoColumnsData = twoColumnsData.concat(batchTwoColumnsData);
+    count++;
+    if (count % 1000 === 0) {
+      console.log(`Processed ${count} items`);
     }
+  }
 
-    const json2csvParser = new Parser();
-    const csv = json2csvParser.parse(consolidatedData);
+  csvStream.end();
+  twoColumnsCsvStream.end();
 
-    fs.writeFileSync(outputCsvPath, csv, 'utf8');
-    console.log('CSV file has been saved as all.csv');
+  console.log('CSV files have been saved');
 
-    const json2csvTwoColumnsParser = new Parser({ fields: ['saashub_url', 'Website'] });
-    const csvTwoColumns = json2csvTwoColumnsParser.parse(twoColumnsData);
-
-    fs.writeFileSync(outputTwoColumnsCsvPath, csvTwoColumns, 'utf8');
-    console.log('CSV file has been saved as all_two_columns.csv');
-
-    // Zip the CSV files
-    const output = fs.createWriteStream(outputZipPath);
+  await new Promise((resolve) => {
+    const output = createWriteStream(outputZipPath);
     const archive = archiver('zip', {
-        zlib: { level: 9 }
+      zlib: { level: 9 }
     });
 
     output.on('close', () => {
-        console.log(`all.zip has been created, total bytes: ${archive.pointer()}`);
+      console.log(`all.zip has been created, total bytes: ${archive.pointer()}`);
+      resolve();
     });
 
     archive.on('error', (err) => {
-        throw err;
+      throw err;
     });
 
     archive.pipe(output);
     archive.file(outputCsvPath, { name: 'all.csv' });
     archive.file(outputTwoColumnsCsvPath, { name: 'all_two_columns.csv' });
     archive.finalize();
-});
+  });
+}
+
+processData().catch(console.error);
